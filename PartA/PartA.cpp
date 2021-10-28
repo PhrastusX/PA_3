4ac85aa1460d2a429c67d901317310d2ffb94cd4
Theophrastus <tgamboni@uccs.edu>
2021-10-27 20:50:39 -0600

    finished part A need to add output files

--- PartA/PartA.cpp
+++ PartA/PartA.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
 
 
-  ifstream in_file("pa3_input_5.txt");
+  ifstream in_file("testfile.txt");
 
   //input vectors
   vector <double> v1;
@@ -35,11 +37,11 @@ int main()
   //build A matrix from input vectors
   double A[2][2] = { {v1[0],v2[0]}, {v1[1],v2[1]} };
 
-  //find first row in output matrix
+  //find first row in A` matrix
   double x1 = S[0][0] * A[0][0] + S[0][1] * A[1][0];
   double x2 = S[0][0] * A[0][1] + S[0][1] * A[1][1];
 
-  //find second row in output matrix
+  //find second row in A` matrix
   double x3 = S[1][0] * A[0][0] + S[1][1] * A[1][0];
   double x4 = S[1][0] * A[0][1] + S[1][1] * A[1][1];
 
@@ -55,12 +57,30 @@ int main()
   //test to see if the system is Undertermined or Inconsistent
   //executes if vectors in A are linearally dependent
   if (A_prime[1][0] == 0 && A_prime[1][1] == 0){
+    
     if (b_prime[1] == 0){
       cout << "System Underdetermined" << endl;
+
     }
     else{
       cout << "System Inconsistent" << endl;
     }
+    cout << "non trivial solution exists" << endl;
+    //calculate a solution
+    double u_two = 1;
+
+    double u_one = - A_prime[0][1]/A_prime[0][0];
+
+    //normalize
+    cout << u_one << endl;
+    cout << u_two << endl;
+    double length = sqrt(pow(u_one,2) + pow(u_two,2));
+
+    cout << fixed << setprecision(4) << u_one/length << " " << u_two/length << endl;
+
+    //print normalized vector
+
+    
   } 
 
   //compute u vector
@@ -74,6 +94,7 @@ int main()
   double u[2] = {u_one, u_two}; 
 
   cout << fixed << setprecision(4) << "u_one: " << u_one << "\nu_two: "<< u_two << endl;
+  cout << "Only Trivial Solution exists" << endl;
 }
 
 
