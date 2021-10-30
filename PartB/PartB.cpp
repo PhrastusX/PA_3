50370456bbd40c56d0d122376835628b3e8246ef
PhrastusX <the_diehl@live.com>
2021-10-30 16:37:37 -0600

    got partb working for the most part need to add colomn pivot

--- PartB/PartB.cpp
+++ PartB/PartB.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 void check_array(double A[4]);
+void mult2x2(double A_prime[4], double mat[4], double S[4]);
+double calc_length(double a, double b);
+bool is_same_mat(double mat1[4], double mat2[4]);
+
+
 int main() 
 {
 
@@ -15,6 +20,7 @@ int main()
 
   //Read file and store in A
   ifstream in_file("testfile.txt");
+  ofstream out_file("testfile_out.txt");
 
   while (!in_file.eof()){
 
@@ -34,10 +40,6 @@ int main()
   A2x2[2] = A[3];
   A2x2[3] = A[4];
 
-  for (int i = 0; i < 4; i++){
-    cout << A2x2[i] << endl;
-  }
-
   //find the characteristic equation
 
   //coeffient on lambda squared will always be 1
@@ -50,14 +52,14 @@ int main()
   //of anti diagonals
   double c = A[0] * A[4] - A[1] * A[3];
 
-  cout << a << " " << b << " " << c << endl;
+  
 
   //calculate roots
   //root one is dominant
   double root1 = (-b + sqrt(pow(b,2) - 4*a*c))/2.0;
   double root2 = (-b - sqrt(pow(b,2) - 4*a*c))/2.0;
 
-  cout << root1 << " " << root2 << endl;
+
 
   //find dominant eigen value
 
@@ -87,8 +89,6 @@ int main()
    
 
   }
-  cout << mat2[0] << " " << mat2[1] << "\n" << mat2[2] << " " << mat2[3] << endl;
-
   //find r1 and r2 using mat1 and mat2
 
   //gauss elimination to mat1 and mat2
@@ -103,28 +103,101 @@ int main()
   S1[1] = 0;
   S2[1] = 0;
 
-  S1[2] = - mat1[2]/mat1[0];
-  S2[2] = - mat2[2]/mat2[2];
+  S1[2] = - (mat1[2]/mat1[0]);
+  S2[2] = - (mat2[2]/mat2[0]);
 
   S1[3] = 1;
   S2[3] = 1;
 
-  cout << S2[0] << " " << S2[1] << "\n" << S2[2] << " " << S2[3] << endl;
-
+  
   //multiply sheer matrix by mat and mat2 
   double A_prime1[4];
   double A_prime2[4];
 
-  A_prime1[0] = S1[0] * mat1[0] + S1[1] * mat1[2];
-  A_prime1[1] = S1[0] * mat1[1] + S1[1] * mat1[3];
+  mult2x2(A_prime1, mat1, S1);
+  mult2x2(A_prime2, mat2, S2);
 
-  A_prime1[2] = S1[2] * mat1[0] + S1[3] * mat1[2];
-  A_prime1[3] = S1[2] * mat1[1] + S1[3] * mat1[3];
 
-  check_array(A_prime1);
+ 
+
+  //back substitution
+  double u11 =  1;
+  double u12 = - A_prime1[0]/A_prime1[1];
+  double length1 = calc_length(u11, u12);
+
+  double u21 = 1;
+  double u22 = - A_prime2[0]/A_prime2[1];
+  double length2 = calc_length(u21, u22);
+
+  double R[4] = {u11/length1, u21/length2, u12/length1, u22/length2};
+  
+
+  double R_t[4] = {R[0],R[2],R[1], R[3]};
+  double lambda[4] = {root1, 0, 0 , root2};
+
+  double test[4];
+  double test1[4];
+  mult2x2(test, R_t, lambda);
+  mult2x2(test1, test, R);
+  
+
+  out_file << lambda[0] << " " << lambda[1] << "\n" << lambda[2] << " " << lambda[3] << endl;
+  out_file << R[0] << " " << R[1] << "\n" << R[2] << " " << R[3] << endl;
+  out_file << test1[0] <<" " << test1[1] << "\n" << test1[2] << " " << test1[3] << endl;
+  
+  if (is_same_mat(test1, A2x2) ){
+    out_file << "1" <<endl;
+  }
+  else{
+    out_file << "0"<< endl;
+  }
+
+
+}//main
+
+
+
+
+void check_array(double A[4]){
+
+  cout << A[0] << " " << A[1] << "\n" << A[2] << " " << A[3] << endl;
 
 }
 
-void check_array(double A[4]){
-  cout << A[0] << " " << A[1] << "\n" << A[2] << " " << A[3] << endl;
+
+
+void mult2x2(double A_prime[4], double mat[4], double S[4]){
+
+  A_prime[0] = S[0] * mat[0] + S[1] * mat[2];
+  A_prime[1] = S[0] * mat[1] + S[1] * mat[3];
+
+  A_prime[2] = S[2] * mat[0] + S[3] * mat[2];
+  A_prime[3] = S[2] * mat[1] + S[3] * mat[3];
+}
+
+
+double calc_length(double a, double b){
+
+  double length = sqrt(pow(a,2) + pow(b,2));
+  return length;
+}
+
+bool is_same_mat(double mat1[4], double mat2[4]){
+  int val1 = 0;
+  int val2;
+  int val3;
+  int val4;
+
+  for (int i = 0; i<4; i++){
+    if ( abs(mat1[i] - mat2[i]) < .1){
+      val1++;
+    }
+
+  }
+  if (val1 == 4){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
